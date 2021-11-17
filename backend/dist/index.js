8500ba1fe84b08dac1da26ab7f98a05303da4d4d
Bruna Nunes <bruna.nunes@softdesign.com.br>
2021-11-17 16:21:21 -0300

    usando typescript no front - conectando com o backend

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -4,8 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var cors_1 = __importDefault(require("cors"));
+var routes_1 = __importDefault(require("./routes"));
 var app = (0, express_1.default)();
-app.get('/', function (req, res) {
-    res.send('Hello World');
-});
+app.use((0, cors_1.default)());
+app.use(routes_1.default);
 app.listen(3333);
